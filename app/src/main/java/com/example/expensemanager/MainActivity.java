adb4c63c1ec6cc7db0fb56663bd0cbb457d00b68
Gaurav <gauravkh@thoughtworks.com>
2019-11-21 20:14:39 +0530

    [Gaurav] Adds 'add expense button' and shows basic popup with static msg upon click with dissmiss button option.

--- app/src/main/java/com/example/expensemanager/MainActivity.java
+++ app/src/main/java/com/example/expensemanager/MainActivity.java
@@ -3,6 +3,15 @@ package com.example.expensemanager;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.PopupWindow;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import java.util.jar.Attributes;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +19,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+    public void sendMessage(View view) {
+
+        LayoutInflater inflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
+
+        View popup = inflater.inflate(R.layout.popup, null);
+        PopupWindow popupWindow = new PopupWindow(popup, LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+        popupWindow.showAtLocation(popup, Gravity.CENTER, 0, 0);
     }
 }
